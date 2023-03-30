6b4d876b7fde298a21392a2510ee9ffe81ffe627
reinaldoper <reinaldoper83@gmail.com>
2023-03-30 13:44:04 -0400

    test: feito alguns tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
 import { render, screen } from '@testing-library/react';
+/* import { userEvent } from '@testing-library/react'; */
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/calc/i);
+  const mult = screen.getByRole('button', {
+    name: /\*/i
+  });
+  const button3 = screen.getByRole('button', {
+    name: /5/i
+  });
+  const button2 = screen.getByRole('button', {
+    name: /2/i
+  });
+
+
   expect(linkElement).toBeInTheDocument();
+  expect(button2).toBeInTheDocument();
+  /* userEvent.click(button2); */
+  expect(button3).toBeInTheDocument();
+  /* userEvent.click(button3); */
+  expect(mult).toBeInTheDocument();
 });
